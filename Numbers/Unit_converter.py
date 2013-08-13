df5411200d23a984bf3a0a34ce849f4f84d4e7f2
Andrej Znidarsic <andrej.znidarsic@gmail.com>
2013-08-13 14:29:12 +0200

    Added initial version of unit converter

---
+++ Numbers/Unit_converter.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+#""" (temp, currency, volume, mass and more)** – Converts various units between one another. The user enters the type of unit being entered, the type of unit they want to convert to and then the value. The program will then make the conversion."""
+
+unit_one = raw_input("Enter initial unit \n Allowed values are 'ug', 'mg', 'g', 'dg', 'kg', 't', 'um', 'mm', 'cm', 'dm', 'm', 'km', 'um3', 'mm3', 'cm3', 'dm3', 'm3', 'km3'\n:")
